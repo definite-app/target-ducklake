92d85112801a76d92fed37a160b5a9b1a7919d0c
Steven Wang <steven@luabase.com>
2026-03-28 22:03:39 -0400

    wrap inserts and deletes in transactions

--- target_ducklake/connector.py
+++ target_ducklake/connector.py
@@ -448,7 +448,9 @@ class DuckLakeConnector(SQLConnector):
         columns_sql = self._build_columns_sql(file_columns, target_table_columns)
         table_ref = f"{self.catalog_name}.{target_schema_name}.{table_name}"
         insert_sql = f"""
+        BEGIN TRANSACTION;
         INSERT INTO {table_ref} SELECT {columns_sql} FROM '{file_location}';
+        COMMIT;
         """
         logger.info(f"Inserting into table {table_name} with SQL {insert_sql}")
         self.execute(insert_sql)
@@ -497,12 +499,16 @@ class DuckLakeConnector(SQLConnector):
         # See: https://github.com/duckdb/ducklake/pull/906
         table_ref = f"{self.catalog_name}.{target_schema_name}.{table_name}"
         delete_sql = f"""
+        BEGIN TRANSACTION;
         DELETE FROM {table_ref}
         WHERE {key_condition} IN (SELECT {key_condition} FROM '{file_location}');
+        COMMIT;
         """
         insert_sql = f"""
+        BEGIN TRANSACTION;
         INSERT INTO {table_ref}
         SELECT {columns_sql} FROM '{file_location}';
+        COMMIT;
         """
 
         logger.info(f"Executing merge operation on table {table_name}")
